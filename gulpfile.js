3b0192c585fb1adcc8282e45daa5e9b2dd2c67dc
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-31 18:59:26 -0500

    Servidor cambiado a lighttpd en lugar de tornado

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var replace = require('gulp-replace');
 var plumber = require('gulp-plumber');
 var watch = require('gulp-watch');
 var livereload = require('gulp-livereload');
-var BASE_URL = 'http://localhost:8888/static/';
+var BASE_URL = 'http://localhost:81/animalitos/media/';
 var DESTINO = 'media/dist/';
 var MEDIA = 'media/'
 

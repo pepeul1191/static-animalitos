dce43f2840f4c25fc99469a8e48f40e379d4ac50
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-01 17:28:55 -0500

    Cambiando la ubicación de los archivos dist

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ function errorLog(error){
 gulp.task('fonts', function() {
     gulp.src([MEDIA + 'bower_components/font-awesome/fonts/*', MEDIA + 'bower_components/bootstrap/fonts/*', MEDIA + 'assets/fontastic/fonts/*'])
     .pipe(plumber())
-      .pipe(gulp.dest(DESTINO + 'assets'));
+      .pipe(gulp.dest(DESTINO));
 });
 
 gulp.task('layout-css', function() {
@@ -29,17 +29,17 @@ gulp.task('layout-css', function() {
       .pipe(plumber())
       .pipe(concatCss('styles.min.css'))
       .pipe(minifyCss())
-      .pipe(replace('../../../font-awesome/fonts/', BASE_URL + 'dist/assets/'))
-      .pipe(replace('../../../../assets/fontastic/fonts', BASE_URL + 'dist/assets/'))
+      .pipe(replace('../../../font-awesome/fonts/', BASE_URL + 'dist/'))
+      .pipe(replace('../../../../assets/fontastic/fonts', BASE_URL + 'dist/'))
       .pipe(replace('../fonts/glyphicons', 'glyphicons'))
-      .pipe(gulp.dest(DESTINO + 'assets'));
+      .pipe(gulp.dest(DESTINO));
 });
 
 gulp.task('layout-js', function() {
     gulp.src([MEDIA + 'bower_components/jquery/dist/jquery.min.js', MEDIA + 'bower_components/bootstrap/dist/js/bootstrap.min.js', MEDIA + 'bower_components/underscore/underscore-min.js', MEDIA + 'bower_components/backbone/backbone-min.js', MEDIA + 'bower_components/backbone.marionette/lib/backbone.marionette.min.js', MEDIA + 'bower_components/handlebars/handlebars.min.js'])
     .pipe(plumber())
     .pipe(concatJs('libs.min.js'))
-    .pipe(gulp.dest(DESTINO + 'assets'));
+    .pipe(gulp.dest(DESTINO));
 });
 
 gulp.task('layout', ['fonts', 'layout-css', 'layout-js']);
@@ -57,13 +57,13 @@ gulp.task('error-css', function() {
       .pipe(minifyCss())
       .pipe(replace('../../../font-awesome/fonts/', BASE_URL + 'dist/assets/'))
       .pipe(replace('../../../assets/fontastic/fonts/', 'assets/'))
-      .pipe(gulp.dest(DESTINO + 'assets'));
+      .pipe(gulp.dest(DESTINO));
 });
 
 gulp.task('app', function(){
   gulp.start('fonts', 'layout-css', 'layout-js');
   gulp.src([
-          DESTINO + 'assets/libs.min.js',  
+          DESTINO + 'libs.min.js',  
           MEDIA + 'layouts/site.js',  
           MEDIA + 'models/usuario.js', 
           MEDIA + 'views/home.js', 

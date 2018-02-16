94fa95839283b31f3197886316bb94d6c169fab2
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-16 18:48:27 -0500

    Moviendo archivos a la raiz

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,9 @@ var replace = require('gulp-replace');
 var plumber = require('gulp-plumber');
 var watch = require('gulp-watch');
 var livereload = require('gulp-livereload');
-var BASE_URL = 'http://localhost:81/animalitos/media/';
-var DESTINO = 'media/dist/';
-var MEDIA = 'media/'
+var BASE_URL = 'http://localhost:81/animalitos-static/';
+var DESTINO = 'dist/';
+var MEDIA = ''
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function errorLog(error){
@@ -19,27 +19,44 @@ function errorLog(error){
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 gulp.task('fonts', function() {
-    gulp.src([MEDIA + 'bower_components/font-awesome/fonts/*', MEDIA + 'bower_components/bootstrap/fonts/*', MEDIA + 'assets/fontastic/fonts/*'])
-    .pipe(plumber())
-      .pipe(gulp.dest(DESTINO));
+  gulp.src([
+    MEDIA + 'bower_components/font-awesome/fonts/*',
+    MEDIA + 'bower_components/bootstrap/fonts/*',
+    MEDIA + 'assets/fontastic/fonts/*'
+  ])
+  .pipe(plumber())
+  .pipe(gulp.dest(DESTINO));
 });
 
 gulp.task('layout-css', function() {
-      gulp.src([MEDIA + 'bower_components/bootstrap/dist/css/bootstrap.min.css', MEDIA + 'bower_components/font-awesome/css/font-awesome.min.css', MEDIA + 'assets/fontastic/styles.css', MEDIA + 'assets/site/css/styles.css', MEDIA + 'assets/login/index.css'])
-      .pipe(plumber())
-      .pipe(concatCss('styles.min.css'))
-      .pipe(minifyCss())
-      .pipe(replace('../../../font-awesome/fonts/', BASE_URL + 'dist/'))
-      .pipe(replace('../../../../assets/fontastic/fonts', BASE_URL + 'dist/'))
-      .pipe(replace('../fonts/glyphicons', 'glyphicons'))
-      .pipe(gulp.dest(DESTINO));
+  gulp.src([
+    MEDIA + 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+    MEDIA + 'bower_components/font-awesome/css/font-awesome.min.css',
+    MEDIA + 'assets/fontastic/styles.css',
+    MEDIA + 'assets/site/css/styles.css',
+    MEDIA + 'assets/login/index.css'
+  ])
+  .pipe(plumber())
+  .pipe(concatCss('styles.min.css'))
+  .pipe(minifyCss())
+  .pipe(replace('../../../font-awesome/fonts/', BASE_URL + 'dist/'))
+  .pipe(replace('../../../../assets/fontastic/fonts', BASE_URL + 'dist/'))
+  .pipe(replace('../fonts/glyphicons', 'glyphicons'))
+  .pipe(gulp.dest(DESTINO));
 });
 
 gulp.task('layout-js', function() {
-    gulp.src([MEDIA + 'bower_components/jquery/dist/jquery.min.js', MEDIA + 'bower_components/bootstrap/dist/js/bootstrap.min.js', MEDIA + 'bower_components/underscore/underscore-min.js', MEDIA + 'bower_components/backbone/backbone-min.js', MEDIA + 'bower_components/backbone.marionette/lib/backbone.marionette.min.js', MEDIA + 'bower_components/handlebars/handlebars.min.js'])
-    .pipe(plumber())
-    .pipe(concatJs('libs.min.js'))
-    .pipe(gulp.dest(DESTINO));
+  gulp.src([
+    MEDIA + 'bower_components/jquery/dist/jquery.min.js',
+    MEDIA + 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+    MEDIA + 'bower_components/underscore/underscore-min.js',
+    MEDIA + 'bower_components/backbone/backbone-min.js',
+    MEDIA + 'bower_components/backbone.marionette/lib/backbone.marionette.min.js',
+    MEDIA + 'bower_components/handlebars/handlebars.min.js'
+  ])
+  .pipe(plumber())
+  .pipe(concatJs('libs.min.js'))
+  .pipe(gulp.dest(DESTINO));
 });
 
 gulp.task('layout', ['fonts', 'layout-css', 'layout-js']);
@@ -47,37 +64,39 @@ gulp.task('layout', ['fonts', 'layout-css', 'layout-js']);
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 gulp.task('error-css', function() {
-      gulp.src([
-          MEDIA + 'bower_components/bootstrap/dist/css/bootstrap.min.css',
-          MEDIA + 'bower_components/font-awesome/css/font-awesome.min.css', 
-          MEDIA + 'assets/fontastic/styles.css', 
-          MEDIA + 'assets/error/index.css'])
-      .pipe(plumber())
-      .pipe(concatCss('error.min.css'))
-      .pipe(minifyCss())
-      .pipe(replace('../../../font-awesome/fonts/', BASE_URL + 'dist/assets/'))
-      .pipe(replace('../../../assets/fontastic/fonts/', 'assets/'))
-      .pipe(gulp.dest(DESTINO));
+  gulp.src([
+    MEDIA + 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+    MEDIA + 'bower_components/font-awesome/css/font-awesome.min.css',
+    MEDIA + 'assets/fontastic/styles.css',
+    MEDIA + 'assets/error/index.css'
+  ])
+  .pipe(plumber())
+  .pipe(concatCss('error.min.css'))
+  .pipe(minifyCss())
+  .pipe(replace('../../../font-awesome/fonts/', BASE_URL + 'dist/assets/'))
+  .pipe(replace('../../../assets/fontastic/fonts/', 'assets/'))
+  .pipe(gulp.dest(DESTINO));
 });
 
 gulp.task('app', function(){
   gulp.start('fonts', 'layout-css', 'layout-js');
   gulp.src([
-          DESTINO + 'libs.min.js',  
-          MEDIA + 'layouts/site.js',  
-          MEDIA + 'models/usuario.js', 
-          MEDIA + 'views/home.js', 
-          MEDIA + 'views/buscar.js', 
-          MEDIA + 'views/contacto.js',  
-          MEDIA + 'views/registro.js', 
-          MEDIA + 'views/_form_registro.js', 
-          MEDIA + 'views/login.js', 
-          MEDIA + 'views/_form_login.js' , 
-          MEDIA + 'views/_form_contrasenia.js' ,
-          MEDIA + 'routes/router.js'])
-    //.pipe(uglify())
-    .pipe(plumber())
-    .pipe(concatJs('app.min.js'))
-    .pipe(gulp.dest(DESTINO))//.pipe(gulp.dest(DESTINO + 'home'))
-    .pipe(livereload());
+    DESTINO + 'libs.min.js',
+    MEDIA + 'layouts/site.js',
+    MEDIA + 'models/usuario.js',
+    MEDIA + 'views/home.js',
+    MEDIA + 'views/buscar.js',
+    MEDIA + 'views/contacto.js',
+    MEDIA + 'views/registro.js',
+    MEDIA + 'views/_form_registro.js',
+    MEDIA + 'views/login.js',
+    MEDIA + 'views/_form_login.js' ,
+    MEDIA + 'views/_form_contrasenia.js' ,
+    MEDIA + 'routes/router.js'
+  ])
+  //.pipe(uglify())
+  .pipe(plumber())
+  .pipe(concatJs('app.min.js'))
+  .pipe(gulp.dest(DESTINO))//.pipe(gulp.dest(DESTINO + 'home'))
+  .pipe(livereload());
 });
